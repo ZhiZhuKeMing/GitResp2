a12c3a2d5edd249aacd2e902b90c73324fe850ad
zhukeming <523828085@qq.com>
2022-05-16 17:25:26 +0800

    第四次进行数据的提交。

--- src/edu/hyjsxx/Person.java
+++ src/edu/hyjsxx/Person.java
@@ -4,13 +4,12 @@ package edu.hyjsxx;
  * This program display an XML document as a tree in JSON format
  *
  * @auther Zhukeming
- * @Data: 2022 - 05 - 16 - 17:17
+ * @Data: 2022 - 05 - 16 - 17:24
  * @Description: edu.hyjsxx
  * @Version 2.01
  */
-public class Teaher {
+public class Person {
     private int age;
     private String name;
     private String sex;
-
 }
