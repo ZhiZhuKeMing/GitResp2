a5bab0d710f9f76ae9f4d5d1b242540f01f569a9
zhukeming <523828085@qq.com>
2022-05-16 15:57:47 +0800

    第二次进行数据的提交。

---
+++ src/Person.java
@@ -0,0 +1,12 @@
+/**
+ * This program display an XML document as a tree in JSON format
+ *
+ * @auther Zhukeming
+ * @Data: 2022 - 05 - 16 - 15:56
+ * @Description: PACKAGE_NAME
+ * @Version 2.01
+ */
+public class Person {
+    private int age;
+    private String name;
+}
