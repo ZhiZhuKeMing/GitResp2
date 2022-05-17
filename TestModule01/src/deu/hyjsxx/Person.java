dca1dfdb0f93f02610484bc5f428629b352a0d7a
zhukeming <523828085@qq.com>
2022-05-17 09:14:25 +0800

    第八次进行数据的提交。

---
+++ TestModule01/src/deu/hyjsxx/Person.java
@@ -0,0 +1,15 @@
+package deu.hyjsxx;
+
+/**
+ * This program display an XML document as a tree in JSON format
+ *
+ * @auther Zhukeming
+ * @Data: 2022 - 05 - 17 - 9:12
+ * @Description: deu.hyjsxx
+ * @Version 2.01
+ */
+public class Person {
+    private int age;
+    private String name;
+    private String sex;
+}
